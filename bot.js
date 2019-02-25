543b2e76e46d545d135d61909509e832cabce6ba
VLVSV <40785126+VLVSV@users.noreply.github.com>
2019-02-24 20:09:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,8 @@ client.on("message", (message) => {
   if (message.author.bot) return;
   if (message.channel.type !== "text") return;
   
+  console.log(message.author.id);
+  
   if((message.content.toLowerCase().indexOf("strike") || message.content.toLowerCase().indexOf("anne") !== -1) && message.author.id.indexOf("211603633286938624") !== -1) {
     message.channel.send("You're gonna see what I'm made of!", {files: ["https://shadowverse-portal.com/image/card/en/C_900334040.png?20190218a"]});
   }
